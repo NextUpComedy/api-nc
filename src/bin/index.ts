7823a6d671b7087482b578ecb5b801006849a0e4
aymansami98 <afahgan@students.iugaza.edu.ps>
2023-04-13 10:36:05 +0100

    update ready to upload

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,10 +1,43 @@
 import { createServer } from 'http';
 import { getDashboardSettings, sequelize } from 'nc-db-new';
 import { dto } from '../helpers';
+import { IServerAddress } from '../interfaces';
 import Logger from '../helpers/logger';
 import app from '../app';
+import config from '../config';
 
+const normalizePort = (val: string): number | string | boolean => {
+  const port = parseInt(val, 10);
+  if (Number.isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+};
+
+const port = normalizePort(config.server.PORT);
+app.set('port', port);
 const server = createServer(app);
+
+const onError = (error: NodeJS.ErrnoException): void => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
+  switch (error.code) {
+    case 'EACCES':
+      Logger.error(`${bind} requires elevated privileges`);
+      throw new Error();
+    case 'EADDRINUSE':
+      Logger.error(`${bind} is already in use`);
+      throw new Error();
+    default:
+      throw error;
+  }
+};
 
 (async (): Promise<void> => {
   try {
@@ -12,9 +45,19 @@ const server = createServer(app);
     const dbSettings = await getDashboardSettings();
     const appSettings = dto.generalDTO.toAppSettingsDTO(dbSettings);
     app.set('settings', appSettings);
-    Logger.info('Server listening on port 5000 ...');
+    const onListening = (): void => {
+      const addr: string | IServerAddress | null = server.address();
+      const bind = typeof addr === 'string'
+        ? `pipe ${addr}`
+        : addr !== null
+          ? `${addr.address}:${addr.port}`
+          : `port ${addr}`;
+      Logger.info(`Listening on http://${bind}`);
+    };
 
-    server.listen(8000, '0.0.0.0');
+    server.listen(port as number, '0.0.0.0');
+    server.on('listening', onListening);
+    server.on('error', onError);
   } catch (err) {
     throw new Error(`Unable to connect to the database: ${err}`);
   }
